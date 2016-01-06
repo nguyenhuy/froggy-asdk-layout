575b0d68f084239b681b96e1a10bff835161e475
Huy Nguyen <allforone1511@gmail.com>
2016-01-06 23:39:37 +0200

    Update docs.js

--- js/docs.js
+++ js/docs.js
@@ -3,7 +3,7 @@ var docs = {
     'en': '<p>Aligns items along the cross axis.</p><code>ASStackLayoutAlignItemsStart</code> <code>ASStackLayoutAlignItemsEnd</code> <code>ASStackLayoutAlignItemsCenter</code> <code>ASStackLayoutAlignItemsStretch</code> <code>ASStackLayoutAlignItemsBaselineFirst</code> <code>ASStackLayoutAlignItemsBaselineLast</code>'
   },
   'ASStackLayoutAlignSelf': {
-    'en': '<p>Aligns an item along the cross axis, overriding the <code>ASStackLayoutAlignItems</code> value of the parent stack.</p><code>ASStackLayoutAlignSelfAuto</code> <code>ASStackLayoutAlignSelfStart</code> <code>ASStackLayoutAlignSelfEnd</code> <code>ASStackLayoutAlignSelfCenter</code> <code>ASStackLayoutAlignSelfStretch</code>'
+    'en': '<p>Aligns an item along the cross axis, overriding the <code>alignItems</code> value of the parent stack.</p><code>ASStackLayoutAlignSelfAuto</code> <code>ASStackLayoutAlignSelfStart</code> <code>ASStackLayoutAlignSelfEnd</code> <code>ASStackLayoutAlignSelfCenter</code> <code>ASStackLayoutAlignSelfStretch</code> <code>ASStackLayoutAlignSelfBaselineFirst</code> <code>ASStackLayoutAlignSelfBaselineLast</code>'
   },
   'ASStackLayoutDirection': {
     'en': '<p>Defines the direction of the stack.</p><code>ASStackLayoutDirectionVertical</code> <code>ASStackLayoutDirectionHorizontal</code>'
