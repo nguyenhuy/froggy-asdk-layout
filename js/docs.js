fef1e6aed42e522376ec002d5d8d742ddd451830
Huy Nguyen <allforone1511@gmail.com>
2016-01-06 20:02:41 +0200

    Update docs.js

--- js/docs.js
+++ js/docs.js
@@ -1,6 +1,6 @@
 var docs = {
   'ASStackLayoutAlignItems': {
-    'en': '<p>Aligns items along the cross axis.</p><code>ASStackLayoutAlignItemsStart</code> <code>ASStackLayoutAlignItemsCenter</code> <code>ASStackLayoutAlignItemsEnd</code> <code>ASStackLayoutAlignItemsStretch</code> <code>ASStackLayoutAlignItemsBaselineFirst</code> <code>ASStackLayoutAlignItemsBaselineLast</code>'
+    'en': '<p>Aligns items along the cross axis.</p><code>ASStackLayoutAlignItemsStart</code> <code>ASStackLayoutAlignItemsEnd</code> <code>ASStackLayoutAlignItemsCenter</code> <code>ASStackLayoutAlignItemsStretch</code> <code>ASStackLayoutAlignItemsBaselineFirst</code> <code>ASStackLayoutAlignItemsBaselineLast</code>'
   },
   'ASStackLayoutAlignSelf': {
     'en': '<p>Aligns an item along the cross axis, overriding the <code>ASStackLayoutAlignItems</code> value of the parent stack.</p><code>ASStackLayoutAlignSelfAuto</code> <code>ASStackLayoutAlignSelfStart</code> <code>ASStackLayoutAlignSelfEnd</code> <code>ASStackLayoutAlignSelfCenter</code> <code>ASStackLayoutAlignSelfStretch</code>'
@@ -9,6 +9,6 @@ var docs = {
     'en': '<p>Defines the direction of the stack.</p><code>ASStackLayoutDirectionVertical</code> <code>ASStackLayoutDirectionHorizontal</code>'
   },
   'ASStackLayoutJustifyContent': {
-    'en': '<p>Aligns items along the main axis if there is available space.</p><code>ASStackLayoutJustifyContentStart</code> <code>ASStackLayoutJustifyContentCenter</code> <code>ASStackLayoutJustifyContentEnd</code> <code>ASStackLayoutJustifyContentSpaceBetween</code> <code>ASStackLayoutJustifyContentSpaceAround</code>'
+    'en': '<p>Aligns items along the main axis if there is available space.</p><code>ASStackLayoutJustifyContentStart</code> <code>ASStackLayoutJustifyContentEnd</code> <code>ASStackLayoutJustifyContentCenter</code> <code>ASStackLayoutJustifyContentSpaceBetween</code> <code>ASStackLayoutJustifyContentSpaceAround</code>'
   },
 };
