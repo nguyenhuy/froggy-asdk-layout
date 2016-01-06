39ad72bbf6b7760c3492ad74af6098a1c638062d
Huy Nguyen <allforone1511@gmail.com>
2016-01-06 19:28:52 +0200

    Remove unsupported languages

--- js/docs.js
+++ js/docs.js
@@ -1,114 +1,26 @@
 var docs = {
   'align-content': {
-    'en': '<p>Aligns a flex container\'s lines within the flex container when there is extra space on the cross-axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'de': '<p>Richtet die Zeilen innerhalb eines flex-Containers aus, wenn auf der Querachse Platz ist.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'pt-br': '<p>Alinha as linhas dentro de um container flex quando há espaço sobrando no eixo cruzado.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'es': '<p>Alinea las líneas de un contenedor flex cuando exista más espacio en el eje transversal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'fr': '<p>Aligne les lignes d\'un conteneur flex à l\'intérieur du conteneur flex quand il y a de l\'espace superflu sur l\'axe transversal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'ru': '<p>Выравнивает ряды flex-контейнера внутри него (работает только, если элементы расположены больше чем в один ряд).</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'fa': '<p>فرض کنید که ارتفاع flex-container بیشتر از فضایی است که flex-lineها گرفته اند و اصطلاحا مقداری فضای خالی در flex-container وجود دارد. به کمک خاصیت align-content شما می‌توانید محل قرارگیری این flex-line ها را مشخص کنید که از نظر محور عمودی در کجا قرار گیرند.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'zh-cn': '<p>当交叉轴有多余空间时，对齐容器内的轴线。</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'tr': '<p>Eğer cross-axis\'de fazladan alan varsa, flex container satırlarını flex container\'ın içinde hizalar.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'it': '<p>Allinea le linee all\'interno di un contenitore flex se c\'è extra spazio verticale</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'ko': '<p>세로선 상에 여분의 공간이 있는 경우 Flex 컨테이너 사이의 간격을 조절합니다.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>',
-    'lt': '<p>Lygiuoja elastiško supančio elemento eilutes, kai kertančios ašies atžvilgiu yra laisvos vietos.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>'
+    'en': '<p>Aligns a flex container\'s lines within the flex container when there is extra space on the cross-axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>'
   },
   'align-items': {
-    'en': '<p>Aligns flex items along the cross axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'de': '<p>Richtet die flex-Elemente entlang der Querachse aus.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'pt-br': '<p>Alinha itens flex ao longo do eixo cruzado.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'es': '<p>Alinea elementos Flex a lo largo del eje transversal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'fr': '<p>Aligne les éléments flex selon l\'axe transversal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'ru': '<p>Выравнивает flex-элементы вдоль пересекаемой оси.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'fa': '<p>این خاصیت نحوه چینش Flex itemها را در محور Cross مشخص می کند. این خاصیت مانند justify-content  است تنها تفاوت آنها در مقادیرشان است.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'zh-cn': '<p>在交叉轴上对齐多个元素</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'tr': '<p>Flex öğelerini cross-axis boyunca hizalar.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'it': '<p>Allinea elementi flex lungo l\'asse verticale</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'ko': '<p>Flex 요소를 세로선 상에서 정렬합니다.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'lt': '<p>Lygiuoja elastiškus elementus palei kertančią ašį.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
+    'en': '<p>Aligns flex items along the cross axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
   },
   'align-self': {
-    'en': '<p>Aligns a flex item along the cross axis, overriding the <code>align-items</code> value.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'de': '<p>Richtet die flex-Elemente entlang der Querachse aus, und &uuml;berschreibt den Wert von <code>align-items</code>.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'pt-br': '<p>Alinha um item flex ao longo do eixo cruzado, substituindo o valor de <code>align-items</code>.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'es': '<p>Alinea un elemento flex a lo largo del eje transversal, anulando el valor de <code>align-items</code></p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'fr': '<p>Aligne un élément flex selon l\'axe transversal, outrepassant la valeur de <code>align-items</code>.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'ru': '<p>Выравнивает flex-элемент вдоль пересекаемой оси, перекрывая значения свойства <code>align-items</code>.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'fa': '<p>با استفاده از این خاصیت می‌توانید نحوه قرار گرفتن عمودی هر flex item را مشخص کنید. مقادیر این خاصیت دقیقا همان مقادیر خاصیت align-items است. <code>align-items</code> value.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'zh-cn': '<p>在交叉轴上对齐一个元素，覆盖<code>align-items</code>所设定的值.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'tr': '<p><code>align-items</code>\'ın değerini ezerek, sadece bu öğeyi cross-axis boyunca hizalar</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'it': '<p>Allinea un elemento flex lungo l\'asse verticale, sovrascrive il valore di <code>align-items</code>.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'ko': '<p>지정된 <code>align-items</code> 값을 무시하고 Flex 요소를 세로선 상에서 정렬합니다.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>',
-    'lt': '<p>Lygiuoja elastišką elementą palei kertančią ašį nepaisant <code>align-items</code> reikšmės.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
+    'en': '<p>Aligns a flex item along the cross axis, overriding the <code>align-items</code> value.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
   },
   'flex-direction': {
-    'en': '<p>Defines the direction of the main axis.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'de': '<p>Legt die Ausrichtung der Hauptachse fest.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'es': '<p>Define la dirección del eje principal.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'pt-br': '<p>Define a direção do eixo principal.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'fr': '<p>Définit la direction de l\'axe principal.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'ru': '<p>Задает направление основной оси.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'fa': '<p>جهت آیتم ها را مشخص می کند.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'zh-cn': '<p>定义主轴的方向。</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'tr': '<p>main axis\'in yönünü belirler.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'it': '<p>Definisce la direzione dell\'asse principale</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'ko': '<p>정렬할 방향을 지정합니다.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>',
-    'lt': '<p>Nustato pagrindinės ašies kryptį.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>'
+    'en': '<p>Defines the direction of the main axis.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>'
   },
   'flex-flow': {
-    'en': '<p>Shorthand property for <code>flex-direction</code> and <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'de': '<p>Kurzschreibweise f&uuml;r <code>flex-direction</code> und <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'pt-br': '<p>Propriedade abreviada para <code>flex-direction</code> e <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'es': '<p>Abreviación de las propiedades de <code>flex-direction</code> and <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code></p>',
-    'fr': '<p>Raccourci pour <code>flex-direction</code> et <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'ru': '<p>Свойство для быстрой записи <code>flex-direction</code> и <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'fa': '<p>اکنون به کمک flex-flow شما می‌توانید مقادیری که برای <code>flex-direction</code> و <code>flex-wrap</code> در نظر دارید را در یک خاصیت بنویسید.</p>',
-    'zh-cn': '<p><code>flex-direction</code>和<code>flex-wrap</code>的缩写性质.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'tr': '<p><code>flex-direction</code> ve <code>flex-wrap</code> için kısa yazım özelliği.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'it': '<p>Scorciatoia per le proprieta\' <code>flex-direction</code> e <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'ko': '<p>다음의 속성들을 간략히 한 속성입니다: <code>flex-direction</code> and <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>',
-    'lt': '<p>Sutrumpinta komanda atstojanti <code>flex-direction</code> ir <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>'
+    'en': '<p>Shorthand property for <code>flex-direction</code> and <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>'    
   },
   'flex-wrap': {
-    'en': '<p>Specifies whether flex items are forced on a single line or can be wrapped on multiple lines.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'de': '<p>Legt fest, ob die flex-Element in eine Zeile gezw&auml;ngt werden oder auf mehrere Zeilen umgebrochen werden k&ouml;nnen.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'pt-br': '<p>Especifica se os itens flex são forçados em um linha única ou podem quebrar em múltiplas linhas.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'es': '<p>Especifica si los elementos flex son forzados a una sola línea o pueden ser envueltos en multiples líneas.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'fr': '<p>Spécifie si les éléments flex sont forcés à être sur une seule ligne ou peuvent être enveloppés sur plusieurs lignes.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'ru': '<p>Указывает, нужно ли чтоб элементы принудительно находились в одном ряду или автоматически переносились.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'fa': '<p>در حالت عادی همه flex item ها در یک خط قرار می‌گیرند. به کمک خاصیت flex-wrap شما می‌توانید مشخص کنید که flex item ها در چندین خط قرار بگیرند.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'zh-cn': '<p>定义flex元素必须在单行或自动换行成多行。</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'tr': '<p>Flex öğelerin tek satıra sığmasını ya da çoklu satırlara bölünmesini belirler.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'it': '<p>Specifica se gli elementi flex vengono forzati su una singola linea oppure possono occupare piu\' linee</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'ko': '<p>Flex 요소들을 한 줄 또는 여러 줄에 걸쳐 정렬합니다.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>',
-    'lt': '<p>Nusako ar elastiški elementai turi būti sutalpinti vienoje eilutėje, ar gali būti paskirstyti per kelias eilutes.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>'
+    'en': '<p>Specifies whether flex items are forced on a single line or can be wrapped on multiple lines.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>'
   },
   'justify-content': {
-    'en': '<p>Aligns flex items along the main axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'de': '<p>Richtet die flex-Elemente entlang der Hauptachse aus.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'pt-br': '<p>Alinha os itens flex ao longo do eixo principal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'es': '<p>Alinea los elementos flex a lo largo del eje principal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'fr': '<p>Aligne les éléments flex selon l\'axe principal.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'ru': '<p>Выравнивает flex-элементы вдоль главной оси.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'fa': '<p>با استفاده از این خاصیت می‌توانید نحوه چیدمان آیتم‌ها بر روی محور اصلی را تعیین کنید (به یاد داشته باشید همانطور که گفتیم اگر خاصیت flex-direction برابر column یا column-reverse باشد، محور اصلی عمودی خواهد بود).</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'zh-cn': '<p>将flex元素和主轴对齐。</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'tr': '<p>Flex öğeleri main axis boyunca hizalar.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'it': '<p>Allinea elementi flex lungo l\'asse principale.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'ko': '<p>Flex 요소들을 가로선 상에서 정렬합니다.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>',
-    'lt': '<p>Lygiuoja elastiškus elementus palei pagrindinę ašį.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>'
+    'en': '<p>Aligns flex items along the main axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>'
   },
   'order': {
-    'en': '<p>Specifies the order of the flex item.</p><code>&lt;integer&gt;</code>',
-    'de': '<p>Legt die Reihenfolge der flex-Elemente fest.</p><code>&lt;integer&gt;</code>',
-    'pt-br': '<p>Especifica a ordem de um item flex.</p><code>&lt;integer&gt;</code>',
-    'es': '<p>Especifica el orden de los elementos flex.</p><code>&lt;integer&gt;</code>',
-    'fr': '<p>Spécifie l\'ordre de l\'élément flex.</p><code>&lt;integer&gt;</code>',
-    'ru': '<p>Указывает порядок flex-элемента.</p><code>&lt;integer&gt;</code>',
-    'fa': '<p>به کمک خاصیت order شما می‌توانید ترتیب چینش Flex itemها را مشخص کنید. مقادیر این خاصیت اعداد می‌باشند و هرچه عدد کوچکتر باشد اولویت آیتم بالاتر خواهد بود..</p><code>&lt;integer&gt;</code>',
-    'zh-cn': '<p>决定flex元素的顺序.</p><code>&lt;integer&gt;</code>',
-    'tr': '<p>Flex öğenin sırasını belirler.</p><code>&lt;integer&gt;</code>',
-    'it': '<p>Specifica l\'ordine degli elementi flex</p><code>&lt;integer&gt;</code>',
-    'ko': '<p>Flex 요소의 순서를 지정합니다.</p><code>&lt;integer&gt;</code>',
-    'lt': '<p>Nusako elastiškų elementų eilės tvarką.</p><code>&lt;integer&gt;</code>'
+    'en': '<p>Specifies the order of the flex item.</p><code>&lt;integer&gt;</code>'
   }
 };
--- js/messages.js
+++ js/messages.js
@@ -1,101 +1,23 @@
 var messages = {
   next: {
-    'en': 'Next',
-    'de': 'Weiter',
-    'fr': 'Suivant',
-    'pt-br': 'Próximo',
-    'es': 'Siguiente',
-    'ru': 'Следующий',
-    'fa': 'مرحله بعدی',
-    'zh-cn': '继续',
-    'tr': 'Sonraki',
-    'it': 'Prossima',
-    'ko': '다음',
-    'lt': 'Sekantis'
+    'en': 'Next'
   },
   labelFooter: {
-    'en': 'Flexbox Froggy is created by',
-    'de': 'Flexbox Froggy wurde gemacht von',
-    'fr': 'Flexbox Froggy est créé par',
-    'pt-br': 'Flexbox Froggy foi criado por',
-    'es': 'Flexbox Froggy es una creación de',
-    'ru': 'Игра Flexbox Froggy создана',
-    'fa': 'قورباغه فلکس باکس ساخته شده توسط',
-    'zh-cn': 'Flexbox Froggy的作者是',
-    'tr': 'Flexbox Froggy\'nin yaratıcısı:',
-    'it': 'Flexbox Froggy e\' stato creato da',
-    'ko': 'Flexbox Froggy는 다음에 의해 개발되었습니다',
-    'lt': 'Flexbox Froggy kūrėjas'
+    'en': 'Flexbox Froggy is created by'
   },
   labelLevel: {
-    'en': 'Level',
-    'de': 'Level',
-    'fr': 'Niveau',
-    'pt-br': 'Nível',
-    'es': 'Nivel',
-    'ru': 'Уровень',
-    'fa': 'مرحله',
-    'zh-cn': 'Level',
-    'tr': 'Seviye',
-    'it': 'Livello',
-    'ko': '단계',
-    'lt': 'Lygis'
+    'en': 'Level'
   },
   labelOf: {
-    'en': 'of',
-    'de': 'von',
-    'fr': 'de',
-    'pt-br': 'de',
-    'es': 'de',
-    'ru': 'из',
-    'fa': 'از',
-    'zh-cn': '/',
-    'tr': '/',
-    'it': 'di',
-    'ko': 'of',
-    'lt': 'iš'
+    'en': 'of'
   },
   title: {
-    'en': 'Flexbox Froggy - A game for learning CSS flexbox',
-    'de': 'Flexbox Froggy - Ein Spiel, um CSS flexbox zu lernen',
-    'fr': 'Flexbox Froggy - Un jeu pour apprendre les flexbox CSS',
-    'pt-br': 'Flexbox Froggy - Um jogo para aprender CSS flexbox',
-    'es': 'Flexbox Froggy - Un juego para aprender CSS flexbox',
-    'ru': 'Flexbox Froggy - Игра для изучения CSS Flexbox',
-    'fa': 'قورباغه فلکس باکس - یک بازی جذاب برای آموزش سی اس اس فلکس باکس',
-    'zh-cn': 'Flexbox Froggy - 一个用来学CSS flexbox的游戏',
-    'tr': 'Flexbox Froggy - CSS flexbox için öğretici oyun',
-    'it': 'Flexbox Froggy - Un gioco per imparare CSS flexbox',
-    'fa': 'قورباغه فلکس باکس - یک باز برای آموزش سی اس اس فبکس باکس',
-    'ko': 'Flexbox Froggy - CSS flexbox 속성 배우기 게임',
-    'lt': 'Flexbox Froggy - Žaidimas padėsiantis perprasti CSS "flexbox" metodiką'
+    'en': 'Flexbox Froggy - A game for learning CSS flexbox'
   },
   labelReset: {
-    'en': 'Reset',
-    'de': 'Zurücksetzen',
-    'fr': 'Réinitialiser',
-    'pt-br': 'Reinicializar',
-    'es': 'Reiniciar',
-    'ru': 'Сбросить',
-    'fa': 'شروع مجدد',
-    'zh-cn': '重置',
-    'tr': 'Baştan Başla',
-    'it': 'Annulla',
-    'ko': '재시작',
-    'lt': 'Atstatyti'
+    'en': 'Reset'
   },
   warningReset: {
-    'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
-    'de': 'Bist su sicher, dass du das Spiel zurücksetzen möchtest?\n\nDein gespeicherter Fortschritt geht dabei verloren und du musst das Spiel von neuem starten.',
-    'fr': 'Êtes-vous certain de vouloir réinitialiser la partie?\n\nVotre progrès sauvegardé sera perdu et vous recommencerez au début du jeu.',
-    'pt-br': 'Você tem certeza que quer reinicializar o jogo?\n\nSeu progresso salvo será perdido e você voltará ao começo do jogo.',
-    'es': 'Estás seguro, ¿Quieres reiniciar el juego?\n\nTu progreso se perderá y serás enviado al inicio del juego.',
-    'ru': 'Вы действительно хотите сбросить игру?\n\nВаш прогресс будет потерян и вы вернетесь к началу игры.',
-    'fa': 'آیا از شروع مجدد بازی اطمینان دارید ?\n\nتمام مراحل ذخیره شده حذف خواهند شد.',
-    'zh-cn': '你确定要重置游戏吗?\n\n你保存了的进度将会丢失，而且你会回到游戏的最开始。',
-    'tr': 'Oyuna baştan başlamak istediğinize emin misiniz? Kayıtlı bütün ilerlemeniz silinecek ve birinci seviyeye geri döneceksiniz.',
-    'it': 'Sei sicuro di voler annullare il gioco?\n\nI tuoi salvataggi verranno persi e dovrai ricominciare il gioco dall\'inizio.',
-    'ko': '게임을 다시 시작하시겠습니까? 지금까지의 진도는 사라지고 게임의 처음으로 돌아갈 것입니다.',
-    'lt': 'Ar esate įsitikinę, kad norite atstatyti žaidimą į pradinę būseną?\n\nJūsų pasiekimai bus anuliuoti ir būsite nukreipti į žaidimo pradžią.'
+    'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.'
   }
 };
