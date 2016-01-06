0b4203c1567efd23dacc75c631eaa8b07154d66f
Huy Nguyen <allforone1511@gmail.com>
2016-01-06 19:42:55 +0200

    Define properties of ASStackLayoutSpec

--- js/docs.js
+++ js/docs.js
@@ -1,26 +1,14 @@
 var docs = {
-  'align-content': {
-    'en': '<p>Aligns a flex container\'s lines within the flex container when there is extra space on the cross-axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code> <code>stretch</code>'
+  'ASStackLayoutAlignItems': {
+    'en': '<p>Aligns items along the cross axis.</p><code>ASStackLayoutAlignItemsStart</code> <code>ASStackLayoutAlignItemsCenter</code> <code>ASStackLayoutAlignItemsEnd</code> <code>ASStackLayoutAlignItemsStretch</code> <code>ASStackLayoutAlignItemsBaselineFirst</code> <code>ASStackLayoutAlignItemsBaselineLast</code>'
   },
-  'align-items': {
-    'en': '<p>Aligns flex items along the cross axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
+  'ASStackLayoutAlignSelf': {
+    'en': '<p>Aligns an item along the cross axis, overriding the <code>ASStackLayoutAlignItems</code> value of the parent stack.</p><code>ASStackLayoutAlignSelfAuto</code> <code>ASStackLayoutAlignSelfStart</code> <code>ASStackLayoutAlignSelfEnd</code> <code>ASStackLayoutAlignSelfCenter</code> <code>ASStackLayoutAlignSelfStretch</code>'
   },
-  'align-self': {
-    'en': '<p>Aligns a flex item along the cross axis, overriding the <code>align-items</code> value.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>baseline</code> <code>stretch</code>'
+  'ASStackLayoutDirection': {
+    'en': '<p>Defines the direction of the stack.</p><code>ASStackLayoutDirectionVertical</code> <code>ASStackLayoutDirectionHorizontal</code>'
   },
-  'flex-direction': {
-    'en': '<p>Defines the direction of the main axis.</p><code>row</code> <code>row-reverse</code> <code>column</code> <code>column-reverse</code>'
+  'ASStackLayoutJustifyContent': {
+    'en': '<p>Aligns items along the main axis if there is available space.</p><code>ASStackLayoutJustifyContentStart</code> <code>ASStackLayoutJustifyContentCenter</code> <code>ASStackLayoutJustifyContentEnd</code> <code>ASStackLayoutJustifyContentSpaceBetween</code> <code>ASStackLayoutJustifyContentSpaceAround</code>'
   },
-  'flex-flow': {
-    'en': '<p>Shorthand property for <code>flex-direction</code> and <code>flex-wrap</code>.</p><code>&lt;flex-direction&gt; &lt;flex-wrap&gt;</code>'    
-  },
-  'flex-wrap': {
-    'en': '<p>Specifies whether flex items are forced on a single line or can be wrapped on multiple lines.</p><code>nowrap</code> <code>wrap</code> <code>wrap-reverse</code>'
-  },
-  'justify-content': {
-    'en': '<p>Aligns flex items along the main axis.</p><code>flex-start</code> <code>flex-end</code> <code>center</code> <code>space-between</code> <code>space-around</code>'
-  },
-  'order': {
-    'en': '<p>Specifies the order of the flex item.</p><code>&lt;integer&gt;</code>'
-  }
 };
