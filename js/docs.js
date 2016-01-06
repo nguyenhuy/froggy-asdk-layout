d09ae4beb560a629e9d48f567fb0a13d6cbba894
Huy Nguyen <allforone1511@gmail.com>
2016-01-07 00:25:43 +0200

    Fix tooltips not showing when hovering over property names

--- js/docs.js
+++ js/docs.js
@@ -1,14 +1,14 @@
 var docs = {
-  'ASStackLayoutAlignItems': {
+  'alignItems': {
     'en': '<p>Aligns items along the cross axis.</p><code>ASStackLayoutAlignItemsStart</code> <code>ASStackLayoutAlignItemsEnd</code> <code>ASStackLayoutAlignItemsCenter</code> <code>ASStackLayoutAlignItemsStretch</code> <code>ASStackLayoutAlignItemsBaselineFirst</code> <code>ASStackLayoutAlignItemsBaselineLast</code>'
   },
-  'ASStackLayoutAlignSelf': {
+  'alignSelf': {
     'en': '<p>Aligns an item along the cross axis, overriding the <code>alignItems</code> value of the parent stack.</p><code>ASStackLayoutAlignSelfAuto</code> <code>ASStackLayoutAlignSelfStart</code> <code>ASStackLayoutAlignSelfEnd</code> <code>ASStackLayoutAlignSelfCenter</code> <code>ASStackLayoutAlignSelfStretch</code> <code>ASStackLayoutAlignSelfBaselineFirst</code> <code>ASStackLayoutAlignSelfBaselineLast</code>'
   },
-  'ASStackLayoutDirection': {
+  'direction': {
     'en': '<p>Defines the direction of the stack.</p><code>ASStackLayoutDirectionVertical</code> <code>ASStackLayoutDirectionHorizontal</code>'
   },
-  'ASStackLayoutJustifyContent': {
+  'justifyContent': {
     'en': '<p>Aligns items along the main axis if there is available space.</p><code>ASStackLayoutJustifyContentStart</code> <code>ASStackLayoutJustifyContentEnd</code> <code>ASStackLayoutJustifyContentCenter</code> <code>ASStackLayoutJustifyContentSpaceBetween</code> <code>ASStackLayoutJustifyContentSpaceAround</code>'
   },
 };
